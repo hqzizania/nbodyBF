e2aadcb5b02934fac13f7b41cfcb44401607a35f
hqzizania <qian.huang@intel.com>
2014-12-25 22:00:34 +0800

    prepare for bigDS

--- src/test/com/intel/nbody/GenLattice.scala
+++ src/test/com/intel/nbody/GenLattice.scala
@@ -12,32 +12,32 @@ import scala.util.Random
 /**
  * @param dirPath The directory path of an input file recording crystal coordination system
  */
-class GenLattice(sc: SparkContext, nparticles:Int, slices:Int, dirPath: String)
-  extends GenLatticeExample(sc, nparticles, slices){
+class GenLattice(sc: SparkContext, nParticles:Int, slices:Int, dirPath: String)
+  extends GenLatticeExample(sc, nParticles, slices){
 
 }
 
 /**
  * Generate a simple cubic lattice lattice taking an example of solid argon
  * @param sc  SparkContext
- * @param nparticles number of particles in a molecular or crystal
+ * @param nParticles number of particles in a molecular or crystal
  * @param slices  number of slices for spark running
  */
-class GenLatticeExample(sc: SparkContext, nparticles:Int, slices:Int){
+class GenLatticeExample(sc: SparkContext, nParticles:Int, slices:Int){
   val rand = new Random(42)
   def generateData = {
     var x: mutable.Set[Array[Array[Double]]] = mutable.Set.empty
     var iii = 0
-    val nn = ceil(pow(nparticles, 1.0/3)).toInt
-    val L = pow(nparticles/0.8, 1.0/3)  // linear size of cubical volume
+    val nn = ceil(pow(nParticles, 1.0/3)).toInt
+    val L = pow(nParticles/0.8, 1.0/3)  // linear size of cubical volume
     val pa = L / nn     // lattice spacing
-    val vscale = 0.1    // maximum initial velocity component
+    val vScale = 0.1    // maximum initial velocity component
 
     /** initialize positions
      * (0): x direction, (1): y direction, (2): z direction
      * */
 
-    val lattice = Array.ofDim[Double](nparticles, 3)
+    val lattice = Array.ofDim[Double](nParticles, 3)
     for(k <- 0 until nn){
       for(i <- 0 until nn){
         for(j <- 0 until nn){
@@ -58,8 +58,8 @@ class GenLatticeExample(sc: SparkContext, nparticles:Int, slices:Int){
      * */
 
      while (x.size < slices) {
-      val y = Array.ofDim[Double](nparticles/slices, 10)
-      for (i <- 0 until nparticles/slices){
+      val y = Array.ofDim[Double](nParticles/slices, 10)
+      for (i <- 0 until nParticles/slices){
         y(i)(0)= iii
         y(i)(1)= lattice(iii)(0)
         y(i)(2)= lattice(iii)(1)
@@ -68,9 +68,9 @@ class GenLatticeExample(sc: SparkContext, nparticles:Int, slices:Int){
         y(i)(5)= 0.0
         y(i)(6)= 0.0
 
-        y(i)(7)= (rand.nextDouble() * 2 - 1) * vscale
-        y(i)(8)= (rand.nextDouble() * 2 - 1) * vscale
-        y(i)(9)= (rand.nextDouble() * 2 - 1) * vscale
+        y(i)(7)= (rand.nextDouble() * 2 - 1) * vScale
+        y(i)(8)= (rand.nextDouble() * 2 - 1) * vScale
+        y(i)(9)= (rand.nextDouble() * 2 - 1) * vScale
 
         iii += 1
 
